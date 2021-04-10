f3fffc5d92df5bf25a965183d9e69736996468a1
tmy628 <tmy6h1r@gmail.com>
2021-04-10 05:15:39 +0000

    [Add] 複数の関数で表示できるようにする 複数の関数を定義

--- app.js
+++ app.js
@@ -1,6 +1,65 @@
-var promptStr = prompt('何か好きな文字を入力してください。');
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
-alert(promptStr);
+// じゃんけんの手をランダムに作成する関数を呼び出す
+var js_hand = getJShand();
 
-// ファイルを保存し、index.htmlファイルをダブルクリックして開くと入力ダイアログが表示されるので、好きな文字を入力する
-// 入力してOKボタンをクリックすると、入力された文字列はprompt()関数の戻り値として変数「promptStr」に格納され、アラートウィンドウ上に表示される
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+var judge = winLose(user_hand, js_hand);
+
+// 結果を表示する
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  var js_hand_num = Math.floor( Math.random() * 3 );
+  // 乱数を生成する関数を設定している これにより、グー・チョキ・パーがランダムに表示されるようになる
+　// Math.floor( Math.random() * 3 )は、0～2までのランダムな整数値を生成する
+　// 関数内の「3」は、ランダム生成したい整数値の+1を指定している
+  var hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+  // ランダム生成された数値を基に条件分岐させ、グー・チョキ・パーを出し分ける
+
+  return hand_name;
+  // 最後に関数の戻り値として、グー・チョキ・パーの文字列を返す
+}
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  var winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
