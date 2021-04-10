a8295211632f3ccc19791e74812e74e64367eace
tmy628 <tmy6h1r@gmail.com>
2021-04-10 00:33:53 +0000

    [Add] 作成した関数を呼び出す

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-// 「関数（function）」は、繰り返し使われる一連の処理を1つにまとめたもの
-// JavaScriptでは、functionに続けて関数名を書き、{ }内に処理内容を記述する
+// メイン部分
+var alertString;
+alertString = addString("WebCamp");
 
-// function 関数名(引数){
-//  処理
-//  return 戻り値;
-// }
+// 作成した関数を呼び出す
+alert(alertString);
 
+// 作成した関数
 function addString(strA){
-// functionの後に、関数名「addString」を記述し、引数として「strA」を定義している
   var addStr = "Hello " + strA;
-  // 変数名「addStr」に、「Hello 」の文字列と、関数の引数「strA」に設定されている文字列を結合している
   return addStr;
-  // 結合結果の格納された「addStr」を、関数の戻り値としている returnを記述することで、出力結果が表示される
 }
